2cc0cda0b2ea76bfc96327fd444a62e33fa196f1
Andrew Hartwell <arhartwe@ucsc.edu>
2018-11-17 01:23:51 -0800

    implemented code for notification and .js file on firebase. Notification not displaying currently,looking for fix.

--- app/src/main/java/com/example/brandon/quicktrip/holders/ItemViewHolder.java
+++ app/src/main/java/com/example/brandon/quicktrip/holders/ItemViewHolder.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.text.InputType;
@@ -16,8 +17,12 @@ import com.example.brandon.quicktrip.R;
 import com.example.brandon.quicktrip.activities.GroceryListActivity;
 import com.example.brandon.quicktrip.models.GroceryList;
 import com.example.brandon.quicktrip.models.Item;
+import com.example.brandon.quicktrip.models.NotificationModel;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -35,7 +40,7 @@ public class ItemViewHolder extends RecyclerView.ViewHolder {
         createdBy = v.findViewById(R.id.createdBy);
     }
 
-    public void populateItem(Context context, View groceryListView, String userEmail, GroceryList groceryList, Item item) {
+    public void populateItem(Context context, View groceryListView, String userEmail, String userName, GroceryList groceryList, Item item) {
         String groceryListID = groceryList.getListID();
         String groceryListName = groceryList.getListName();
         String itemID = item.getItemID();
@@ -65,7 +70,27 @@ public class ItemViewHolder extends RecyclerView.ViewHolder {
                 @Override
                 public void onSuccess(Void aVoid) {
                     if(show) {
-                        //Send Notification
+                        rootref.collection("shoppingLists").document(userEmail)
+                                .collection("userShoppingLists").document(groceryListID).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                Map<String, Object> map = (Map<String, Object>) task.getResult().get("users");
+                                String notificationMessage = userName + " has deleted  " + itemName + " from " + groceryListName + "'s list";
+                                NotificationModel notificationModel = new NotificationModel(notificationMessage, userEmail);
+
+                                if (map != null) {
+                                    for(Map.Entry<String, Object> entry : map.entrySet()){
+                                        String sharedUserEmail = entry.getKey();
+
+                                        if(!sharedUserEmail.equals(userEmail)){
+                                            rootref.collection("notification").document(sharedUserEmail)
+                                                    .collection("userNotifications").document()
+                                                    .set(notificationModel);
+                                        }
+                                    }
+                                }
+                            }
+                        });
                     }
                 }
             });
